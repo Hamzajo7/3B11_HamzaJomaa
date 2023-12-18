2534aeb164269309bb4052fb61f902fa50aa6a20
Your Name <you@example.com>
2023-12-18 20:28:29 +0100

    last commit

--- js/functions-min.js
+++ js/functions-min.js
@@ -1,3 +1,17 @@
+/*var contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function (event) {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if (!name || !email || !message) {
+        alert("Veuillez remplir tous les champs obligatoires.");
+        event.preventDefault(); // Prevent form submission
+    }
+});
+
+
 /*!
  * VERSION: 1.19.0
  * DATE: 2016-07-16
